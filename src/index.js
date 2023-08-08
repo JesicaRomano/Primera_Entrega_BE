d5e5c371e9020e132e3db11dfeb5cdcbf0d5fcdc
jesica.romano@yahoo.com.ar <jesica.romano@yahoo.com.ar>
2023-08-07 22:16:08 -0300

    subida de primera entrega

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import ProductRouter from "./routes/products.router.js";
+import CartRouter from "./routes/carts.router.js"
+
+const app = express();
+const PORT = 8080;
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+
+app.use("/api/products", ProductRouter)
+app.use("/api/carts", CartRouter)
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express Puerto ${PORT}`);
+})
